6971386a69309702dccd9bb1d0a75ff461cd48e6
Jefwillems <jefwillems100@gmail.com>
2018-05-29 17:38:23 +0200

    init

---
+++ src/components/Contact.vue
@@ -0,0 +1,8 @@
+<template>
+    <p>This is the contact page</p>
+</template>
+<script>
+export default {}
+</script>
+<style lang="scss" scoped>
+</style>
---
+++ src/components/Navigation.vue
@@ -0,0 +1,15 @@
+<template>
+    <nav>
+        <ul>
+            <li><router-link to="/">home</router-link></li>
+            <li><router-link to="/contact">contact</router-link></li>
+        </ul>
+    </nav>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+</style>
